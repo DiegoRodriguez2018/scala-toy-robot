e1031911552861f8c3b2dec67aa86071a43af0bd
Diego Rodriguez <diego.rodriguez@rea-group.com>
2020-02-24 13:22:45 +1100

    Add sbt.util in build.sbt

---
+++ build.sbt
@@ -0,0 +1,9 @@
+import sbt.util
+
+name := "toyrobot"
+
+version := "0.3.1"
+
+scalaVersion := "2.13.1"
+
+
---
+++ src/main/scala/toyrobot/Main.scala
@@ -0,0 +1,7 @@
+package toyrobot
+
+object Main {
+  def main(args: Array[String]): Unit = {   
+    println("Hello")
+  }
+}
